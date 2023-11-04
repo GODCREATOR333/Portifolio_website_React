d82539ce2a846d57be2d61b7c3b8116686071f76
GODCREATOR333 <preetham.chitti.9@gmail.com>
2023-11-05 00:31:55 +0530

    added a notification bar that i am looking for work

--- src/screens/Hero.js
+++ src/screens/Hero.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTypewriter } from 'react-simple-typewriter';
 import image from 'D:\\ALL__BUILDS\\cs-builds\\react-builds\\portifolio-website\\src\\images\\image.png';
 import "./herostyles.css"
 
 function Hero() {
-
+    const [showNotification, setShowNotification] = useState(true);
     const [text] = useTypewriter({
         words: [' am a Designer', ' am a Developer', ' am an Engineer', ' like to build cool stuff'],
         loop: {},
@@ -12,8 +12,29 @@ function Hero() {
 
     });
 
+    const closeNotification = () => {
+        setShowNotification(false);
+    };
+
+    useEffect(() => {
+        const closeButton = document.querySelector('.close');
+        closeButton.addEventListener('click', closeNotification);
+
+        return () => {
+            closeButton.removeEventListener('click', closeNotification);
+        };
+    }, []); // Adding an empty dependency array to run this effect only once
+
+
+
     return (
         <div className='hero_bg'>
+            {showNotification && (
+                <div className="notification">
+                    <span className="close">&times;</span>
+                    I am currently looking for an <span style={{ fontWeight: 'bolder' }}>Internship</span> opportunity.
+                </div>
+            )}
             <div className='heros_hero'>
                 <img
                     className='hi_img'
@@ -29,7 +50,7 @@ function Hero() {
                 </h4>
                 <div className='buttons'>
                     <button className='contact_button'>Get in Touch </button>
-                    <button className='resume'>Resume <span style={{ fontSize: '18px' }}> ⤓ </span></button>
+                    <button className='resume'>Resume <span style={{ fontSize: '18px' }}> ⬇ </span></button>
                 </div>
             </div>
         </div >
