56748b906273f47ccba16d639b8698e6b23c565e
GODCREATOR333 <preetham.chitti.9@gmail.com>
2023-11-05 01:49:41 +0530

    added the fucntionality for resume button

--- src/screens/Backdrop.js
+++ src/screens/Backdrop.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./backdrop.css"
 
+
+
 function BackDrop() {
+
+    const [showNotification, setShowNotification] = useState(true);
+    const closeNotification = () => {
+        setShowNotification(false);
+    };
+
+    useEffect(() => {
+        const closeButton = document.querySelector('.close');
+        closeButton.addEventListener('click', closeNotification);
+
+        return () => {
+            closeButton.removeEventListener('click', closeNotification);
+        };
+    }, []); // Adding an empty dependency array to run this effect only once
+
+
     return (
         <div id='top' className='backdrop'>
+            <div>
+                {showNotification && (
+                    <div className="notification">
+                        <span className="close">&times;</span>
+                        I am currently looking for an <span style={{ fontWeight: 'bolder' }}>Internship</span> opportunity.
+                    </div>
+                )}</div>
             <div className='gradient-overlay1' />
             <div className='backdrop_cover_img1' />
             <div className='gradient-overlay2' />
--- src/screens/Hero.js
+++ src/screens/Hero.js
@@ -4,37 +4,13 @@ import image from 'D:\\ALL__BUILDS\\cs-builds\\react-builds\\portifolio-website\
 import "./herostyles.css"
 
 function Hero() {
-    const [showNotification, setShowNotification] = useState(true);
     const [text] = useTypewriter({
         words: [' am a Designer', ' am a Developer', ' am an Engineer', ' like to build cool stuff'],
         loop: {},
-
-
     });
-
-    const closeNotification = () => {
-        setShowNotification(false);
-    };
-
-    useEffect(() => {
-        const closeButton = document.querySelector('.close');
-        closeButton.addEventListener('click', closeNotification);
-
-        return () => {
-            closeButton.removeEventListener('click', closeNotification);
-        };
-    }, []); // Adding an empty dependency array to run this effect only once
-
-
 
     return (
         <div className='hero_bg'>
-            {showNotification && (
-                <div className="notification">
-                    <span className="close">&times;</span>
-                    I am currently looking for an <span style={{ fontWeight: 'bolder' }}>Internship</span> opportunity.
-                </div>
-            )}
             <div className='heros_hero'>
                 <img
                     className='hi_img'
@@ -50,7 +26,9 @@ function Hero() {
                 </h4>
                 <div className='buttons'>
                     <button className='contact_button'>Get in Touch </button>
-                    <button className='resume'>Resume <span style={{ fontSize: '18px' }}> ⬇ </span></button>
+                    <a href="/my_resume.pdf" target="_blank" rel="noopener noreferrer">
+                        <button className='resume'>Resume <span style={{ fontSize: '18px' }}> ↗ </span></button>
+                    </a>
                 </div>
             </div>
         </div >
