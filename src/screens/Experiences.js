f9e3ac26fa17d9ee3fbe0a117bf74a304159523e
GODCREATOR333 <preetham.chitti.9@gmail.com>
2023-11-06 00:46:14 +0530

    completed experince upto 90 % still only buttons left also adjusted images and h4 in the same div

--- src/screens/Experiences.js
+++ src/screens/Experiences.js
@@ -5,24 +5,31 @@ function Experiences() {
     return (
         <div id='experiences' className='experiences'>
             <h1>Experiences</h1>
-            <div className='containers'>
+            <div className=' containers'>
                 <div className='container_glass'>
-                    <h4>Writing Python Code to calculate complex AeroThermodynamic effects on MIRV's</h4>
-                    <p>Developed Python-based solutions to address critical aerodynamic heating challenges in MIRVs
-                        (Multiple independently targetable reentry vehicles) at DRDL. Transformed trajectory calculations and
-                        aerodynamic heating assessment by creating optimized code using NumPy, SciPy, and advanced
-                        numerical methods. Improved accuracy and efficiency significantly compared to previous MATLAB
-                        code. Enhanced the code's versatility to simulate various conditions, providing crucial insights into skin
-                        temperature and transient heat behavior, which were previously unattainable</p>
+                    <div className='header-container'>
+                        <h4>Writing Python Code to calculate complex AeroThermodynamic effects on MIRV's</h4>
+                        <img className='drdo_img' src='/drdo.png' alt='drdo_img' />
+                    </div>
+                    <ul>
+                        <li>Developed Python solutions using NumPy, SciPy to analyze 13,000+ data points from 25+ parameters for MIRV aerodynamics.</li>
+                        <li>Improved accuracy, efficiency, enabling diverse simulations, revealing skin temperature and heat behavior insights.</li>
+                        <li>Expanded capabilities for varied scenarios, deepening understanding of MIRV performance under different conditions.</li>
+                        <li>Facilitated vital data extraction, offering valuable insights into MIRV aerodynamic effects.</li>
+                    </ul>
 
                 </div>
                 <div className='container_glass'>
-                    <h4>Additive Manufacturing using 3D printing techniques</h4>
-                    <p>I gained proficiency in CAD modeling, including techniques like Stereolithography. I developed skills in
-                        reverse engineering and conducted simulations to optimize models. Additionally, I explored topology
-                        optimization and deepened my understanding of diverse 3D printing technologies and their practical
-                        applications.</p>
-
+                    <div className='header-container'>
+                        <h4>Additive Manufacturing using 3D printing techniques</h4>
+                        <img className=' iitbhu_img' src='/iitbhu.png' alt='iitbhu_img' />
+                    </div>
+                    <ul>
+                        <li>Proficiency in CAD modeling, specifically utilizing Stereolithography techniques for precise model creation.</li>
+                        <li>Applied reverse engineering skills to optimize models through simulation techniques.</li>
+                        <li>Applied reverse engineering skills to optimize models through simulation techniques.</li>
+                        <li>Delved into diverse 3D printing technologies, leveraging practical applications for real-world use based on learned insights.</li>
+                    </ul>
 
                 </div>
             </div>
